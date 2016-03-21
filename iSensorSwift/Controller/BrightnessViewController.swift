a68ab2b606e5ae71e7d378f02071c33cd5dad50e
koogawa <koogawa.app@gmail.com>
2016-03-21 11:42:41 +0900

    Add BrightnessViewController

---
+++ iSensorSwift/Controller/BrightnessViewController.swift
@@ -0,0 +1,33 @@
+//
+//  BrightnessViewController.swift
+//  iSensorSwift
+//
+//  Created by koogawa on 2016/03/21.
+//  Copyright © 2016 koogawa. All rights reserved.
+//
+
+import UIKit
+
+class BrightnessViewController: UIViewController {
+
+    @IBOutlet weak var brightnessLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Action methods
+
+    @IBAction func downButtonDidTap(sender: AnyObject) {
+    }
+
+    @IBAction func upButtonDidTap(sender: AnyObject) {
+    }
+}
