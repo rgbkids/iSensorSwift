ffdf1e8ba85a6a66cae3e220f588c80f1c0bb09d
koogawa <koogawa.app@gmail.com>
2016-03-21 15:23:43 +0900

    Implement BrightnessViewController

--- iSensorSwift/Controller/BrightnessViewController.swift
+++ iSensorSwift/Controller/BrightnessViewController.swift
@@ -15,7 +15,22 @@ class BrightnessViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.updateBrightnessLabel()
+
+        // Observe screen brightness
+        NSNotificationCenter.defaultCenter().addObserver(self,
+                                                         selector: "screenBrightnessDidChange:",
+                                                         name: UIScreenBrightnessDidChangeNotification,
+                                                         object: nil)
+    }
+
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+
+        // Finish observation
+        NSNotificationCenter.defaultCenter().removeObserver(self,
+                                                            name: UIScreenBrightnessDidChangeNotification,
+                                                            object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,11 +38,34 @@ class BrightnessViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Internal methods
+
+    func updateBrightnessLabel() {
+        let screen = UIScreen.mainScreen()
+        self.brightnessLabel.text = "".stringByAppendingFormat("%.2f", screen.brightness)
+    }
+
+    func screenBrightnessDidChange(notification: NSNotification) {
+        if let screen = notification.object {
+            self.brightnessLabel.text = "".stringByAppendingFormat("%.2f", screen.brightness)
+        }
+    }
+
     // MARK: - Action methods
 
     @IBAction func downButtonDidTap(sender: AnyObject) {
+        var brightness = UIScreen.mainScreen().brightness
+        brightness -= 0.1
+        if brightness < 0.0 { brightness = 0.0 }
+        UIScreen.mainScreen().brightness = brightness
+        self.updateBrightnessLabel()
     }
 
     @IBAction func upButtonDidTap(sender: AnyObject) {
+        var brightness = UIScreen.mainScreen().brightness
+        brightness += 0.1
+        if brightness > 1.0 { brightness = 1.0 }
+        UIScreen.mainScreen().brightness = brightness
+        self.updateBrightnessLabel()
     }
 }
