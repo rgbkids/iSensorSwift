7618bbcb8553ace4b327aeaeddd78b1cd7d25ff8
Kosuke Ogawa <koogawa.app@gmail.com>
2016-04-03 12:23:14 +0900

    Develop merge
    * Add SpeedViewController
    
    * Setup Speed storyboard
    
    * Implement SpeedViewController
    
    * Set title to storyboard
    
    * Update layout
    
    * Add launch icon
    
    * Update README.md
    
    * Update README.md

---
+++ iSensorSwift/Controller/SpeedViewController.swift
@@ -0,0 +1,59 @@
+//
+//  SpeedViewController.swift
+//  iSensorSwift
+//
+//  Created by Kosuke Ogawa on 2016/04/03.
+//  Copyright © 2016 koogawa. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class SpeedViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mpsTextField: UITextField!
+    @IBOutlet weak var kphTextField: UITextField!
+
+    var locationManager: CLLocationManager!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.startUpdatingLocation()
+        }
+    }
+
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.stopUpdatingLocation()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - CLLocationManager delegate
+
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        switch status {
+        case .NotDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .Restricted, .Denied:
+            break
+        case .Authorized, .AuthorizedWhenInUse:
+            break
+        }
+    }
+
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        self.mpsTextField.text = "".stringByAppendingFormat("%.2f", newLocation.speed)
+        self.kphTextField.text = "".stringByAppendingFormat("%.2f", newLocation.speed * 3.6)
+    }
+}
