5f50229f0776c75ddcc45719fd66c0d38347e652
koogawa <koogawa.app@gmail.com>
2016-04-09 23:16:45 +0900

    Implement AccelerometerViewController

--- iSensorSwift/Controller/AccelerometerViewController.swift
+++ iSensorSwift/Controller/AccelerometerViewController.swift
@@ -12,9 +12,7 @@ import CoreMotion
 class AccelerometerViewController: UIViewController {
 
     @IBOutlet weak var xLabel: UILabel!
-    
     @IBOutlet weak var yLabel: UILabel!
-    
     @IBOutlet weak var zLabel: UILabel!
     
     let manager = CMMotionManager()
@@ -22,41 +20,34 @@ class AccelerometerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        //取得の間隔
-        manager.accelerometerUpdateInterval = 0.01;
-        
-        // 値取得時にしたい処理を作成
-        let accelerometerHandler:CMAccelerometerHandler = {
-            (data: CMAccelerometerData?, error: NSError?) -> Void in
-            
-            // 取得した値をコンソールに表示
-            self.xLabel.text = "".stringByAppendingFormat("%.2f", data!.acceleration.x)
-            self.yLabel.text = "".stringByAppendingFormat("%.2f", data!.acceleration.y)
-            self.zLabel.text = "".stringByAppendingFormat("%.2f", data!.acceleration.z)
+        if manager.accelerometerAvailable {
+            manager.accelerometerUpdateInterval = 1 / 10; // 10Hz
 
-            print("x: \(data!.acceleration.x) y: \(data!.acceleration.y) z: \(data!.acceleration.z)")
+            let accelerometerHandler:CMAccelerometerHandler = {
+                (data: CMAccelerometerData?, error: NSError?) -> Void in
+
+                self.xLabel.text = "".stringByAppendingFormat("x %.4f", data!.acceleration.x)
+                self.yLabel.text = "".stringByAppendingFormat("y %.4f", data!.acceleration.y)
+                self.zLabel.text = "".stringByAppendingFormat("z %.4f", data!.acceleration.z)
+
+                print("x: \(data!.acceleration.x) y: \(data!.acceleration.y) z: \(data!.acceleration.z)")
+            }
+
+            manager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()!,
+                                                     withHandler: accelerometerHandler)
         }
-        
-        /* 加速度センサーを開始する */
-        manager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()!,
-                                                 withHandler: accelerometerHandler)
+    }
+
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+
+        if manager.accelerometerAvailable {
+            manager.stopAccelerometerUpdates()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
