88f5e22c02525285772b5d2cd907a3a604eeeaeb
koogawa <koogawa.app@gmail.com>
2016-04-23 12:13:48 +0900

    Implement MotionActivityViewController

--- iSensorSwift/Controller/MotionActivityViewController.swift
+++ iSensorSwift/Controller/MotionActivityViewController.swift
@@ -21,21 +21,21 @@ class MotionActivityViewController: UIViewController {
 
     @IBOutlet weak var confidenceLabel: UILabel!
 
-    let stepCounter = CMStepCounter()
     let pedometer = CMPedometer()
-    
+    let activityManager = CMMotionActivityManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        self.stepCounter.startStepCountingUpdatesToQueue(NSOperationQueue.mainQueue(), updateOn: 1, withHandler: { numberOfSteps, timestamp, error in
-            if error == nil {
-//                self.stepLabel.text = "Steps: \(numberOfSteps)"
-            } else {
-                print("Step Counter error: \(error)")
-            }
-        })
         self.startStepCounting()
+        self.startUpdatingActivity()
+    }
+
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+
+        self.pedometer.stopPedometerUpdates()
+        self.activityManager.stopActivityUpdates()
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,38 +43,36 @@ class MotionActivityViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Internal methods
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     func startStepCounting() {
-        // CMPedometerが利用できるか確認
         if CMPedometer.isStepCountingAvailable() {
-            // 計測開始
             self.pedometer.startPedometerUpdatesFromDate(NSDate(), withHandler: {
-                [unowned self] data, error in
+                [weak self] (data: CMPedometerData?, error: NSError?) -> Void in
                 dispatch_async(dispatch_get_main_queue(), {
-                    print("update")
-                    if error != nil {
-                        // エラー
-//                        self.label.text = "エラー : \(error)"
-                        print("エラー : \(error)")
-                    } else {
-                        let lengthFormatter = NSLengthFormatter()
-                        // 歩数
-                        let steps = data?.numberOfSteps
-                        // 結果をラベルに出力
-                        self.stepLabel.text = "Steps: \(data?.numberOfSteps)"
+                    if data != nil && error == nil {
+                        self?.stepLabel.text = "step: \(data!.numberOfSteps)"
                     }
                 })
+            })
+        }
+    }
+
+    func startUpdatingActivity() {
+        if CMMotionActivityManager.isActivityAvailable() {
+            self.activityManager.startActivityUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: {
+                [weak self] (data: CMMotionActivity?) -> Void in
+                dispatch_async(dispatch_get_main_queue(), {
+                    if let data = data {
+                    self?.stationaryLabel.text = "stationary: \(data.stationary)"
+                    self?.walkingLabel.text = "walking: \(data.walking)"
+                    self?.runningLabel.text = "running: \(data.running)"
+                    self?.automotiveLabel.text = "automotive: \(data.automotive)"
+                    self?.unknowLabel.text = "unknown: \(data.unknown)"
+                    self?.confidenceLabel.text = "confidence(0-2): \(data.confidence.rawValue)"
+                    }
                 })
+            })
         }
     }
 }
