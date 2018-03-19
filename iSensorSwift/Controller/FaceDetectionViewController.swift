0b0f5d22781d3440ff18f02b2bc8e8f1472a5a83
rgbkids <rgbkids.g@gmail.com>
2018-03-19 23:55:55 +0900

    commit

--- iSensorSwift/Controller/FaceDetectionViewController.swift
+++ iSensorSwift/Controller/FaceDetectionViewController.swift
@@ -94,4 +94,19 @@ class FaceDetectionViewController: UIViewController, UIImagePickerControllerDele
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
+    
+    // -------------
+    var faceTracker:FaceTracker? = nil;
+    @IBOutlet var cameraView :UIView!//viewController上に一つviewを敷いてそれと繋いでおく
+    
+    var rectView = UIView()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.rectView.layer.borderWidth = 3//四角い枠を用意しておく
+        self.view.addSubview(self.rectView)
+        faceTracker = FaceTracker(view: self.cameraView, findface:{arr in
+            let rect = arr[0];//一番の顔だけ使う
+            self.rectView.frame = rect;//四角い枠を顔の位置に移動する
+        })
+    }
 }
